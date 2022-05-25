b31f9b25fd0b88c7277013662c80d2ecb21f779d
troat9824 <troat9824@gmail.com>
2022-05-25 17:15:35 -0500

    footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+return (
+    <footer className="">
+        <div className="container">
+            &copy;{new Date().getFullYear()} by Franklin Brown, 
+                                                Taylor Vaughn,
+                                                Angel Sanchez,
+                                                and Giovanna Roach
+        </div>
+    </footer>
+    );
+};
+
+export default Footer;
