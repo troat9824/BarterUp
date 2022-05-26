c0fdc589c9589eab6aa2dcba118f97fe3ce875b2
Franklin <franklin.c.brown@gmail.com>
2022-05-26 05:50:35 -0700

    added Home.js foundation

---
+++ client/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ListingList from '../components/ListingList';
+import ListingForm from '../components/ListingForm';
+
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_LISTINGS, QUERY_ME_BASIC } from '../utils/queries';
+
+const Home = () => {
+    const { loading, data } = useQuery(QUERY_LISTINGS);
+    const { data: userData } = useQuery(QUERY_ME_BASIC);
+    const listings = data?.listings || [];
+
+    const loggedIn = Auth.loggedIn();
+
+    return (
+        {/* JSX page template */}
+    )
+};
+
+export default Home;
