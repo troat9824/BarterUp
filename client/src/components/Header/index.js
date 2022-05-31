ea1965d99216b5e88908de028bf42d0ce43e5694
troat9824 <troat9824@gmail.com>
2022-05-31 18:05:16 -0500

    deployed to heroku

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -11,7 +11,7 @@ const Header = () => {
 
     return (
         <header className="bg-secondary mb-4 py-2 flex-row align-center">
-          <div className="text-center">
+          <div className="text-center nav-bar">
             <Link to="/">
               <h1>BarterUp</h1>
             </Link>
@@ -26,10 +26,10 @@ const Header = () => {
                 </>
               ) : (
                 <>
-                  <Link to="/login">Login</Link>
-                  <Link to="/signup">Signup</Link>
-                  <Link to="/profile">Profile</Link>
-                  <Link to="/listings">All Listings</Link>
+                  <Link to="/login"> Login  </Link>
+                  <Link to="/signup"> Signup  </Link>
+                  <Link to="/profile"> Profile  </Link>
+                  <Link to="/listings"> All Listings  </Link>
                 </>
               )}
             </nav>
