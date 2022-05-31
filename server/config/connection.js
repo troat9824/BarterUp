4e8d212080f32493f06ead0adce7a7e0e345bf3c
troat9824 <troat9824@gmail.com>
2022-05-31 12:58:04 -0500

    mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/barterup',
-  {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-  }
+  // process.env.MONGODB_URI,
+  // {
+  //   useNewUrlParser: true, 
+  //   useUnifiedTopology: true,
+  // }
 );
 
 
