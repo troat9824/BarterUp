a4f4798db18d3f316430adad193c4f83ec61a967
troat9824 <troat9824@gmail.com>
2022-05-31 20:16:20 -0500

    profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,81 +5,60 @@ import ListingForm from '../components/ListingForm';
 import AllListings from '../components/AllListings';
 import ListingList from '../components/ListingList';
 
-// import { useQuery, useMutation } from '@apollo/client';
-// import { QUERY_USER, QUERY_ME } from '../utils/queries';
-// import { ADD_FRIEND } from '../utils/mutations';
-// import Auth from '../utils/auth';
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
 
 const Profile = (props) => {
-//   const { username: userParam } = useParams();
+  const { username: userParam } = useParams();
 
-//   const [addFriend] = useMutation(ADD_FRIEND);
-//   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-//     variables: { username: userParam },
-//   });
 
-//   const user = data?.me || data?.user || {};
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
+  });
 
-//   // navigate to personal profile page if username is yours
-//   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-//     return <Navigate to="/profile:username" />;
-//   }
+  const user = data?.me || data?.user || {};
 
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   }
+  // navigate to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile:username" />;
+  }
 
-//   if (!user?.username) {
-//     return (
-//       <h4>
-//         You need to be logged in to see this. Use the navigation links above to
-//         sign up or log in!
-//       </h4>
-//     );
-//   }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-//   const handleClick = async () => {
-//     try {
-//       await addFriend({
-//         variables: { id: user._id },
-//       });
-//     } catch (e) {
-//       console.error(e);
-//     }
-//   };
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
+    );
+  
 
-//   return (
-//     <div>
-//       <div className="flex-row mb-3">
-//         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-//           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-//         </h2>
+  };
 
-//         {userParam && (
-//           <button className="btn ml-auto" onClick={handleClick}>
-//             Add Friend
-//           </button>
-//         )}
-//       </div>
+  return (
+    <div>
+      <div className="flex-row mb-3">
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Welcome Back {userParam ? `${user.username}'s` : 'your'}!.
+        </h2>
+      </div>
 
-//       <div className="flex-row justify-space-between mb-3">
-//         <div className="col-12 mb-3 col-lg-8">
-//           <ThoughtList
-//             thoughts={user.thoughts}
-//             title={`${user.username}'s thoughts...`}
-//           />
-//         </div>
+      <div className="flex-row justify-space-between mb-3">
+        <div className="col-12 mb-3 col-lg-8">
+          <ListingList
+            listings={user.listings}
+            title={`${user.username}'s listings...`}
+          />
+        </div>
 
-//         <div className="col-12 col-lg-3 mb-3">
-//           <FriendList
-//             username={user.username}
-//             friendCount={user.friendCount}
-//             friends={user.friends}
-//           />
-//         </div>
-//       </div>
-//       <div className="mb-3">{!userParam && <ThoughtForm />}</div>
-//     </div>
-//   );
+        
+      </div>
+      <div className="mb-3">{!userParam && <ListingForm />}</div>
+    </div>
+  );
 };
 export default Profile;
