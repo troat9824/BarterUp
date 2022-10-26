cd354127d5a5d9783e3a53a1c1948bfb6df33a9a
troat9824 <troat9824@gmail.com>
2022-10-25 19:55:08 -0500

    current

--- server/models/User.js
+++ server/models/User.js
@@ -36,6 +36,7 @@ const userSchema = new Schema(
 );
 
 // set up pre-save middleware to create password
+// no longer need bcrypt
 userSchema.pre('save', async function(next) {
     if (this.isNew || this.isModified('password')) {
         const saltRounds = 10;
